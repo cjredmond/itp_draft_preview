397906ce1b3bd54b4129f961005b369522a5a145
cjredmond <connor.redmond@gmail.com>
2017-07-30 12:26:20 -0400

    player and report apps

---
+++ player/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+class School(models.Model):
+    name = models.CharField(max_length=100)
+
+class Player(models.Model):
+    name = models.CharField(max_length=100)
+    position = models.CharField(max_length=2)
+    school = models.ForeignKey(School)
+    school_class = models.CharField(max_length=20)
+    age = models.IntegerField()
+    weight = models.IntegerField()
+    height = models.CharField(max_length=100)
+    hometown = models.CharField(max_length=100)
+    highschool = models.CharField(max_length=100)
+    fourty = models.FloatField(null=True,blank=True)
+    ten_yard = models.FloatField(null=True,blank=True)
+    bench = models.FloatField(null=True,blank=True)
+    cone = models.FloatField(null=True,blank=True)
+    short_shut = models.FloatField(null=True,blank=True)
+    sixty = models.FloatField(null=True,blank=True)
+    vertical = models.FloatField(null=True,blank=True)
+    broad = models.FloatField(null=True,blank=True)
